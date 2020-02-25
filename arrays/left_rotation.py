c2b3d348ee58e0fce6887708f994b57c7f0df462
karimation <selmi.kerim@gmail.com>
2020-02-25 19:33:08 +0100

    solve: left rotation

--- arrays/left_rotation.py
+++ arrays/left_rotation.py
@@ -1,5 +1,7 @@
-#### https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
- #!/bin/python3
+####  https://www.hackerrank.com/challenges/ctci-array-left-rotation
+
+#!/bin/python3
+#!/bin/python3
 
 import math
 import os
@@ -7,28 +9,25 @@ import random
 import re
 import sys
 
-def calcul(arr,i,j):
-    return (sum(arr[i-1][j-1:j+2]) + arr[i][j] + sum(arr[i+1][j-1:j+2]))
-# Complete the hourglassSum function below.
-def hourglassSum(arr):
-    res = -100
-    for i in range(1,5):
-        for j in range(1,5):
-            #print(calcul(arr,i,j))
-            res= max(res,calcul(arr,i,j))
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    print(a[d:] + a[:d])
+    return a[d:] + a[:d]
 
-    return res
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    arr = []
+    nd = input().split()
 
-    for _ in range(6):
-        arr.append(list(map(int, input().rstrip().split())))
+    n = int(nd[0])
 
-    result = hourglassSum(arr)
+    d = int(nd[1])
 
-    fptr.write(str(result) + '\n')
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
 
     fptr.close()
-
